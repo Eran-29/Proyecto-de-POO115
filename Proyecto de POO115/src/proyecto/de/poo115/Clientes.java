e30aa37c39b7874958856ec6f96dff097d4ef5a8
erick <erick@Elian>
2025-11-05 10:33:56 -0600

    Prueba de conexión inicial desde NetBeans.

---
+++ Proyecto de POO115/src/proyecto/de/poo115/Clientes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.de.poo115;
+
+/**
+ *
+ * @author erick
+ */
+public class Clientes {
+    
+}
